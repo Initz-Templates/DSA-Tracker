1220069963cfd3ac939e55e368dfd1b7e4b5c429
Manjeet Singh Santoshi <manjeetsingh5186977@gmail.com>
2024-07-04 11:41:11 +0530

    first commit

--- src/components/Lister.js
+++ src/components/Lister.js
@@ -151,10 +151,10 @@ setcount(counter);
           <Link
             target="_blank"
             className=" text-blue-700 font-nunito "
-            to="https://www.linkedin.com/in/Dev-chaitanya-tiwari-a30478200/"
+            to="https://www.linkedin.com/in/manjeet-s-7860b5242/"
           >
             {" "}
-            Dev Chaitanya tiwari &nbsp;
+            Manjeet Singh &nbsp;
           </Link>
 
           <img
@@ -172,9 +172,9 @@ setcount(counter);
           <Link
             target="_blank"
             className="text-sky-600 "
-            to="https://github.com/Dct-tcd"
+            to="https://github.com/samratmanjeet7"
           >
-            &nbsp; Github \ Dct-tcd{" "}
+            &nbsp; Github \ Initializ{" "}
           </Link>
         </div>
       </div>
